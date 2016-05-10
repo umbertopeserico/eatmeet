0c59a2bbdbea5c0a616efbeef2d5db0e4aef5f65
Umberto Peserico <umberto.peserico@gmail.com>
2016-05-10 19:12:50 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+zone = Zone.create(name: 'Zona 1')
+restaurant_owner = RestaurantOwner.create(name:'Ristoratore',
+                                          surname: '1',
+                                          email: 'umberto@peserico.me',
+                                          password:'ristoratore1',
+                                          password_confirmation:'ristoratore1')
+restaurant_owner.confirm!
+
+restaurant = Restaurant.create(name: 'Ristorante 1',
+                               description: 'Descrizione ristorante',
+                               email: 'umberto@peserico.me',
+                               phone: '0445401245',
+                               street: 'Via Prova 10',
+                               city: 'Trento',
+                               zip_code: '38121',
+                               province: 'Trento',
+                               restaurant_owner_id: restaurant_owner.id)
+restaurant.zones << zone
