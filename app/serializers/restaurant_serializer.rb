914b63aca3cd06db81fb059c4934551856ddf80e
Umberto Peserico <umberto.peserico@gmail.com>
2016-06-25 10:23:43 +0200

    Fix missing lat lgt in restaurant serializer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,7 +1,7 @@
 class RestaurantSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :email, :phone,
              :full_address, :street, :city, :zip_code,
-             :province, :restaurant_owner_id,
+             :province, :lat, :lgt, :restaurant_owner_id,
              :created_at, :updated_at
 
   belongs_to :restaurant_owner
